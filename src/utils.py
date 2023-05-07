4a358616e49605dc06794f826ec41e92e14b51fe
mukul74 <agarwal1993mukul@gmail.com>
2023-05-07 21:12:18 +0530

    Topic : HyperTuning in Flow

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,10 @@ import sys
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
+from tqdm import tqdm
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,13 +21,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
-        for i in range(len(list(models))):
+        for i in tqdm(range(len(list(models)))):
             model  = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,param,cv=5)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
@@ -35,7 +42,7 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[list(models.keys())[i]] = (test_model_score,gs.best_params_)
 
         return report
         
